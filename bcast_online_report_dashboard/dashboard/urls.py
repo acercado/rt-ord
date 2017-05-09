f9172ce33a9a65ac2cc5f163d56dd0aed8864c1c
Adz <adzcer@gmail.com>
2017-05-09 11:29:43 +0000

    latest May

--- bcast_online_report_dashboard/dashboard/urls.py
+++ bcast_online_report_dashboard/dashboard/urls.py
@@ -3,7 +3,7 @@ from . import views
 # from allauth.account import views
 
 urlpatterns = [
-    # url(r'/dasboard^$', views.dashboard1, name='dashboard1'),
+    url(r'^$', views.dashboard1, name='dashboard1'),
     # url(r"^accounts/login", views.login, name="account_login"),
     url(r'^panel1/$', views.dashboard1, name='dashboard1'),
     url(r'^panel2/$', views.dashboard2, name='dashboard2'),
